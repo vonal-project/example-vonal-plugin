e9a38e9bc8d137a6b151f988356082e42c43532f
David Judge <biro97@gmail.com>
2019-08-21 02:27:14 +0200

    Reached the first working state

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import { spawn } from 'child_process'
 
 export default (q) => {
     if(q.includes('c'))
-    return <div className="row">
-        Launch 
-        <button onClick={e => {
-            spawn( "/usr/bin/chromium", { detached:true, stdio:'ignore' }).unref()
-        }}>chromium</button>
-    </div>
+        return <div className="row">
+            Launch 
+            <button onClick={e => {
+                // start chrome
+                spawn( "/usr/bin/chromium", { detached:true, stdio:'ignore' }).unref()
+                
+                // hide Vonal
+                global.windowManager.hide()
+            }}>chromium</button>
+        </div>
 
     return null
 }
